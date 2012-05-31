2430d011807f8d2dd5878a4d244392586b46c654
Ben Lowery <ben@blowery.org>
2012-05-31 15:46:32 +0000

    fixup the mac node launcher. avoiding the safari driver for now.

--- start-node-mac.sh
+++ start-node-mac.sh
@@ -1,1 +1,5 @@
-java -jar selenium-server-standalone-2.22.0.jar -role node -nodeConfig node-config-mac.json -hub http://$1/grid/register
+java -jar selenium-server-standalone-2.22.0.jar \
+-role node \
+-hub http://$1/grid/register \
+-browser browserName=firefox,maxInstances=5,platform=MAC \
+-browser browserName=chrome,maxInstances=5,platform=MAC
