ee5c8b1a3a09b3eaa650a54ef2b09b9b738a8cb2
Ben Lowery <ben@blowery.org>
2012-05-31 11:24:32 -0400

    Turns out you can't use nodeConfig for partial config. Move browser config into cmd script.

--- start-node-win.cmd
+++ start-node-win.cmd
@@ -1,2 +1,2 @@
 #call me as start-node-win.cmd hubHost:hubPort
-java -jar selenium-server-standalone-2.22.0.jar -role node -nodeConfig node-config-win.json -hub http://%1/grid/register
+java -jar selenium-server-standalone-2.22.0.jar -role node -browser browserName=firefox,maxInstances=5,platform=WINDOWS -browser browserName=chrome,maxInstances=5,platform=WINDOWS -browser "browserName=internet explorer,maxInstances=1,platform=WINDOWS" -hub http://%1/grid/register
